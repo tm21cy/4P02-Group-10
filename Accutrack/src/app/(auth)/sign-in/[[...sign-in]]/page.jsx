18abd79148c3d3d1231a9b24962adb5e0927c111
Sangmitra <Sangmitra.m06@gmail.com>
2025-02-10 09:34:31 -0500

    Changes to the sign up page with added styles in globals.css and packages

--- Accutrack/src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ Accutrack/src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -7,7 +7,7 @@ export default function Page() {
             <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
             <img
                 alt=""
-                src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                src="https://images.unsplash.com/photo-1579548122080-c35fd6820ecb?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 className="absolute inset-0 h-full w-full object-cover"
             />
             </aside>
